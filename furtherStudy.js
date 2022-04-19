08cd068519ac6453db85ed36b7d7eb909525877c
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-18 19:34:27 -0700

    Completed wordsInCommon() in furtherStudy.js

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,9 +1,26 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+
+  const words1_set = new Set(words1);
+  const words2_set = new Set(words2);
+
+  let result = new Set();
+
+  for (const word of words1_set) {
+    if (words2_set.has(word)) {
+      result.add(word);
+    }
+  }
+
+  result = Array.from(result)
+
+  return result
+
 }
+
 
 function kidsGame(names) {
   // Replace this with your code
 }
+
