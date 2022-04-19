c8dfa32c155061078d8cac3f0c827bd0bdc78aed
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-18 19:22:22 -0700

    Completed objects.js

--- objects.js
+++ objects.js
@@ -2,10 +2,42 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+
+  const words_count = {};
+  
+  const words = phrase.split(' ');
+
+  for (const word of words) {
+    if (word in words_count) {
+      words_count[word] += 1;
+    }
+    else {
+      words_count[word] = 1;
+    }
+  }
+
+  return words_count
+
 }
+
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  
+  const melon_prices = {
+    2.50: ['Cantaloupe', 'Yum', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (melon_prices[price] === undefined) {
+    return
+  }
+
+  return melon_prices[price].sort()
+
 }
+
+
+
